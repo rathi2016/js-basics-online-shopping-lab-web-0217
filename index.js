19f0e409f8dfc48cdcd50f2f7aeb076201f6fd91
Rathi <github email address>
2017-05-25 11:25:03 -0400

    did all other than one of view cart

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
-}
+};
 
 function total() {
   let t = 0
@@ -14,4 +14,60 @@ function total() {
   }
 
   return t
-}
+};
+
+function getCart() {
+  return cart
+};
+
+function addToCart(item) {
+var price =  Math.floor(Math.random() * (100)) ;
+cart.push({[item] : price})
+//  why do we have to make new object with [item] but not {item : price }
+console.log(`${item} has been added to your cart.`)
+return cart
+};
+
+/* Define a function viewCart which does not accept any arguments.
+This function should loop over every item in cart to print out
+"In your cart you have [item and price pairs].". If there isn't anything in your cart,
+the function should print out "Your shopping cart is empty.". */
+
+
+function viewCart() {
+  if (!cart.length) {
+  return console.log("Your shopping cart is empty.")
+  }
+  for (var i = 0,l = cart.length; i<l ; i++){
+    for (var ItemPrice in cart[i][0]) {
+
+       console.log(`In your cart, you have ${cart[i][0]} at ${ItemPrice}.`)
+    }
+  }
+};
+
+//  stack over flow : someArray.splice(x,1);
+function removeFromCart(item) {
+  let x = false
+
+   for (let i = 0, l = cart.length; i < l; i++){
+     if (cart[i].hasOwnProperty(item)) {
+          x = true
+          cart.splice(i)
+     }
+
+   }
+   if (!x){
+      console.log("That item is not in your cart.")
+   }
+   return cart
+};
+
+function placeOrder(cardNumber) {
+  if (!cardNumber) {
+    return console.log("We don't have a credit card on file for you to place your order.")
+  }
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+
+  cart = []
+};
